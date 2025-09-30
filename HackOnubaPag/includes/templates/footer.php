308d6ae81170a5a526887b047475ed04062f1167
Moydehuelva <114821443+Moydehuelva@users.noreply.github.com>
2025-09-30 15:52:31 +0200

    Add files via upload

---
+++ HackOnubaPag/includes/templates/footer.php
@@ -0,0 +1,25 @@
+<footer class="pie-pagina">
+    <div class="contenedor clearfix">
+      <div class="info-pie">
+        <h3>Sobre Hackonuba "no ni ná"</h3>
+        <p>Tras muchos años en busqueda de la perfección sabemos que nos queda muchísimo
+          por hacer pero no cesaremos en buscar la mejor manera de encontrar
+          como ofrecer lo mejor y conseguir ser el punto de encuentro para parners, compradores, empresas, empleadores y aficionados
+        </p>
+      </div>
+      <div class="menu">
+        <h3>Contacto</h3>
+        <nav class="contacto">
+          <a href="mailto:moydehuelva@gmail.com"><img src="img/icons8-correo-50.png" alt="correo"></a>
+          <a href="https://www.linkedin.com/in/moises-gonzalez-lopez" target="_blank"><img src="img/icons8-linkedin-50.png" alt="linkedin"></a>
+        </nav>
+      </div>
+    </div>
+  </footer>
+  
+
+
+
+</body>
+
+</html>
---
+++ HackOnubaPag/includes/templates/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/colorbox.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.colorbox-min.js"></script>
+  <script src="js/main.js"></script>
+  
+
+  <meta name="description" content="">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Antic&family=Antic+Didone&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Add your site or application content here -->
+  <header class="header-site">
+    <div class="hero">
+      <div class="cont-header">
+
+        <div class="description">
+          <div class="clearfix">
+            <p class="fecha">12-13 junio</p>
+            <p class="ubicacion"> Huelva, España</p>
+          </div>
+          <h1 class="nombre-evento">HackOnuba</h1>
+          <p class="lema">Descubriendo el "nuevo mundo" de la <span>Ciberseguridad</span></p>
+        </div>
+
+      </div>
+    </div>
+
+  </header>
+
+
+  <div class="barra">
+    <div class="contenedor clearfix">
+      <div class="logo">
+        <a href="index.php">
+          <img src="img/logo2.png" alt="logo">
+        </a>
+
+      </div>
+      <div class="menu-movil">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="menu clearfix">
+        <a href="index.php">Inicio</a>
+        <a href="charlas.php">Charlas</a>
+        <a href="agenda.php">Agenda</a>
+        <a href="invitados.php">Invitados</a>
+        <a href="Registro.php">Registro</a>
+      </nav>
+    </div>
+  </div><!--fin barra-->
